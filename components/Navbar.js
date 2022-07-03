e273a7853b8a50b3d8986acea71bcef67bb48458
AJAY CHAWARIYA <Ajaychawariya34@gmail.com>
2022-07-03 12:18:45 +0530

    Coffee

---
+++ components/Navbar.js
@@ -0,0 +1,21 @@
+function Navbar(){
+    return
+    `<nav>
+    <i class="fa-solid fa-mug-hot"></i>
+    <ul>
+      <li><a href="index.html">Home</a></li>
+      <li><a href="#">Login</a></li>
+      <li><a href="#">Signup</a></li>
+      <li><a href="bucket.html">Bucket</a></li>
+    </ul>
+   
+    <div  id="coffee_count" <i class="fa-solid fa-cart-shopping"></i> 
+      <!-- <i class="fa-solid fa-cart-shopping"></i>  -->
+      <!--Show total number of coffee added to bucket here, do not add anything only Total count. Not even "Count - 5" but like this "5" -->
+    </div>
+  </nav>
+</div>
+<div id="menu">`
+}
+
+export default Navbar
